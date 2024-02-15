7a5b28b4fda68378b972a342745377b3ffc0399b
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2024-02-15 16:00:54 +0200

    Create 5_Decision_Tree_Regression.py

---
+++ 5_Decision_Tree_Regression.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('Position_Salaries.csv')
+X = df.iloc[:, 1:2]   # Using 1:2 as indices will give us np array of dim (10, 1)
+y = df.iloc[:, 2]
+
+df.head()
+
+
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0).fit(X, y)
+
+plt.scatter(X, y)
+X_grid = np.arange(min(X.values), max(X.values), 0.1).reshape(-1, 1)
+plt.plot(X_grid, regressor.predict(X_grid), color='r')
+plt.show()
+
+
