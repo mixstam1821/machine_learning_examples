4edf2264e82c70f415a2c0f0e84e968271e09483
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2024-02-15 16:08:52 +0200

    Create KNN.py

---
+++ KNN.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('Social_Network_Ads.csv')
+X = df.iloc[:, 2:4]   # Using 1:2 as indices will give us np array of dim (10, 1)
+y = df.iloc[:, 4]
+
+df.head()
+
+# Split in training and testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+
+# Scale
+from sklearn.preprocessing import StandardScaler
+X_sca = StandardScaler()
+X_train = X_sca.fit_transform(X_train)
+X_test = X_sca.transform(X_test)
+
+
+
+
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2) # p = 1 is manhattan distance, p = 2 is Euclidean
+clf.fit(X_train, y_train)
+
+
+
+y_pred = clf.predict(X_test)
+
+
+
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+cm
+
+
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+# generates every pixel in the table. MeshGrid creates one entry for every point from X1 to X2
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01))
+# classifies every pixel as 0 or 1
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c=ListedColormap(('red', 'green'))(i), label=j)
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+plt.title('KNN Classifier')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.show()
+
+
+
